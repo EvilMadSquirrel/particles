e1728b0826ccb13e6e7acb1a6ecd5fea0253d877
EvilMadSquirrel <minichev.s.l@gmail.com>
2023-03-25 16:28:35 +0300

    implemented particle

--- src/main.rs
+++ src/main.rs
@@ -56,11 +56,29 @@ impl World {
     }
 }
 impl Particle {
-    fn new() -> Particle {
-        unimplemented!()
+    fn new(world: &World) -> Particle {
+        let mut rng = thread_rng();
+        let x = rng.gen_range(0.0..=world.width);
+        let y = world.height;
+        let x_velocity = 0.0;
+        let y_velocity = rng.gen_range(-2.0..0.0);
+        let x_acceleration = 0.0;
+        let y_acceleration = rng.gen_range(0.0..0.15);
+
+        Particle {
+            height: 4.0,
+            widht: 4.0,
+            position: [x, y].into(),
+            velocity: [x_velocity, y_velocity].into(),
+            acceleration: [x_acceleration, y_acceleration].into(),
+            color: [1.0, 1.0, 1.0, 0.99],
+        }
     }
-    fn update() {
-        unimplemented!()
+    fn update(&mut self) {
+        self.velocity = add(self.velocity, self.acceleration);
+        self.position = add(self.position, self.velocity);
+        self.acceleration = mul_scalar(self.acceleration, 0.7);
+        self.color[3] *= 0.995;
     }
 }
 fn main() {
