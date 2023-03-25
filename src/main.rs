7cc04f43a3ed3a7e8392ab586a22d33cccf62f08
EvilMadSquirrel <minichev.s.l@gmail.com>
2023-03-25 16:53:32 +0300

    implemented world

--- src/main.rs
+++ src/main.rs
@@ -43,16 +43,58 @@ struct Particle {
 
 impl World {
     fn new(width: f64, height: f64) -> World {
-        unimplemented!()
+        World {
+            current_turn: 0,
+            particles: Vec::<Box<Particle>>::new(),
+            height,
+            width,
+            rng: thread_rng(),
+        }
     }
     fn add_shapes(&mut self, n: i32) {
-        unimplemented!()
+        for _ in 0..n.abs() {
+            let particle = Particle::new(&self);
+            let boxed_particle = Box::new(particle);
+            self.particles.push(boxed_particle);
+        }
     }
-    fn remove_shapes() {
-        unimplemented!()
+    fn remove_shapes(&mut self, n: i32) {
+        for _ in 0..n.abs() {
+            let mut to_delete = None;
+
+            let particle_iter = self.particles.iter().enumerate();
+
+            for (i, particle) in particle_iter {
+                if particle.color[3] < 0.02 {
+                    to_delete = Some(i);
+                }
+                break;
+            }
+
+            if let Some(i) = to_delete {
+                self.particles.remove(i);
+            } else {
+                self.particles.remove(0);
+            };
+        }
     }
-    fn update(&self) {
-        unimplemented!()
+
+    fn update(&mut self) {
+        let n = self.rng.gen_range(-3..=3);
+
+        if n > 0 {
+            self.add_shapes(n);
+        } else {
+            self.remove_shapes(n);
+        }
+
+        self.particles.shrink_to_fit();
+
+        for shape in &mut self.particles {
+            shape.update();
+        }
+
+        self.current_turn += 1;
     }
 }
 impl Particle {
